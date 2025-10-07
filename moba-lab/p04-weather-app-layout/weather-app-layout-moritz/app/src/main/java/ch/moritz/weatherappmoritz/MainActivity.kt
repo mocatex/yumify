8078d2aed3db7161a4d1249a56b82336071c4965
mocatex <moritz@feuchter.email>
2025-10-07 10:45:20 +0200

    Initialize Weather App project with essential files and configurations

---
+++ moba-lab/p04-weather-app-layout/weather-app-layout-moritz/app/src/main/java/ch/moritz/weatherappmoritz/MainActivity.kt
@@ -0,0 +1,47 @@
+package ch.moritz.weatherappmoritz
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import ch.moritz.weatherappmoritz.ui.theme.WeatherAppMoritzTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            WeatherAppMoritzTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    WeatherAppMoritzTheme {
+        Greeting("Android")
+    }
+}
