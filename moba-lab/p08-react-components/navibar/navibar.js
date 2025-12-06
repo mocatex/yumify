710033ee813fa0a1ccf02fcbfb3119677630339b
mocatex <moritz@feuchter.email>
2025-12-06 16:07:00 +0100

    lab08 start files

---
+++ moba-lab/p08-react-components/navibar/navibar.js
@@ -0,0 +1,26 @@
+class CustomNavigationBar extends HTMLElement {
+  constructor() {
+      super()
+      const shadowRoot = this.attachShadow({mode: 'open'})
+      shadowRoot.innerHTML = `
+        <style>   
+          div.navibar {
+            align-items: center;
+            background-color: #ccc;
+            margin: 0;
+            width: 100%;
+            height: 100%;
+          }
+          ::slotted(a) {
+            display: block;
+            text-align: center;
+         }
+        </style>
+        <div class="navibar">
+          <slot></slot>
+        </div>`
+  }
+
+}
+
+customElements.define('navigation-bar', CustomNavigationBar)
